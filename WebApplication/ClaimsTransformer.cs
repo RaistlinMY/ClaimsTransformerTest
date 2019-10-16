cef07ca41b17a01870d3088e5bafe977a929f6e5
Mustafa Yılmaz <mustafa.yilmaz@mypoint.com.tr>
2019-10-16 11:11:39 +0300

    Add files via upload

---
+++ WebApplication/ClaimsTransformer.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace WebApplication
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        public const string IsAdminKey = "IsAdmin";
+
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            var identity = (ClaimsIdentity) principal.Identity;
+
+            // create a new ClaimsIdentity copying the existing one
+            var claimsIdentity = new ClaimsIdentity(
+                identity.Claims,
+                identity.AuthenticationType,
+                identity.NameClaimType,
+                identity.RoleClaimType);
+
+            // check if our user is in the admin table
+            // identity.Name is the domain-prefixed id, eg HOME\philip
+
+            claimsIdentity.AddClaim(
+                new Claim(IsAdminKey, "So say we all"));
+            claimsIdentity.AddClaim(new Claim(
+                claimsIdentity.RoleClaimType,"Administrator"));
+
+
+            // create a new ClaimsPrincipal in observation
+            // of the documentation note
+            return Task.FromResult(new ClaimsPrincipal(claimsIdentity));
+        }
+    }
+}
