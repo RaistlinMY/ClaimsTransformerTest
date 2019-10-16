1b5204aa909bba42ea114868a563179de14ee30d
Mustafa Yılmaz <mustafa.yilmaz@mypoint.com.tr>
2019-10-16 11:24:40 +0300

    Update ClaimsTransformer.cs

--- WebApplication/ClaimsTransformer.cs
+++ WebApplication/ClaimsTransformer.cs
@@ -12,24 +12,18 @@ namespace WebApplication
         {
             var identity = (ClaimsIdentity) principal.Identity;
 
-            // create a new ClaimsIdentity copying the existing one
             var claimsIdentity = new ClaimsIdentity(
                 identity.Claims,
                 identity.AuthenticationType,
                 identity.NameClaimType,
                 identity.RoleClaimType);
 
-            // check if our user is in the admin table
-            // identity.Name is the domain-prefixed id, eg HOME\philip
 
             claimsIdentity.AddClaim(
                 new Claim(IsAdminKey, "So say we all"));
             claimsIdentity.AddClaim(new Claim(
                 claimsIdentity.RoleClaimType,"Administrator"));
 
-
-            // create a new ClaimsPrincipal in observation
-            // of the documentation note
             return Task.FromResult(new ClaimsPrincipal(claimsIdentity));
         }
     }
